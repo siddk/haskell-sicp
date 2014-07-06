72626e152d5126685bb96d26d6aa043e450c36b3
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-05 20:55:16 -0700

    Exercise 1.30

--- chapter1.hs
+++ chapter1.hs
@@ -88,3 +88,17 @@ fastExptIter cur base n = if even n
                           then fastExptIter cur (base * base) (n `div` 2)
                           else fastExptIter (cur * base) base (n - 1)
 
+
+-- Exercise 1.30
+-- The sum procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. Show how to do this by filling in the missing expressions in the following definition:
+-- (define (sum term a next b)
+--   (define (iter a result)
+--     (if <??>
+--         <??>
+--         (iter <??> <??>)))
+--   (iter <??> <??>))
+iterSum :: (Num a, Ord a) => (a -> a) -> a -> (a -> a) -> a -> a
+iterSum term a next b = iter a 0
+    where iter a result = if a > b
+                          then result
+                          else iter (next a) (result + (term a))
