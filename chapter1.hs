48998f5ac54632f867a8f4abea3767e556bc678e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-05 17:41:02 -0700

    Exercise 1.16

--- chapter1.hs
+++ chapter1.hs
@@ -77,3 +77,14 @@ improve :: (Num a, Ord a, Fractional a) => a -> a -> a
 improve guess x = ((x / (guess * guess)) + (2 * guess)) / 3
 
 
+-- Exercise 1.16
+--  Design a procedure that evolves an iterative exponentiation process that uses successive squaring and uses a logarithmic number of steps, as does fast-expt. (Hint: Using the observation that (bn/2)2 = (b2)n/2, keep, along with the exponent n and the base b, an additional state variable a, and define the state transformation in such a way that the product a bn is unchanged from state to state. At the beginning of the process a is taken to be 1, and the answer is given by the value of a at the end of the process. In general, the technique of defining an invariant quantity that remains unchanged from state to state is a powerful way to think about the design of iterative algorithms.)
+fastExpt :: (Num a, Ord a) => a -> Int -> a
+fastExpt base n = fastExptIter 1 base n
+
+fastExptIter :: (Num a, Ord a) => a -> a -> Int -> a
+fastExptIter cur base 0 = cur
+fastExptIter cur base n = if even n
+                          then fastExptIter cur (base * base) (n `div` 2)
+                          else fastExptIter (cur * base) base (n - 1)
+
