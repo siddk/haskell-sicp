7766ea027e776518a7f1996325ae49b53d7f875e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-07 22:08:22 -0700

    2.8 Problem statement

--- chapter2.hs
+++ chapter2.hs
@@ -39,3 +39,4 @@ upperBound interval = snd interval
 
 
 -- Exercise 2.8
+-- Using reasoning analogous to Alyssa's, describe how the difference of two intervals may be computed. Define a corresponding subtraction procedure, called sub-interval.
