6ecf8c07ec833e9e3794636c750d8fa28853b9fa
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-07 20:33:49 -0700

    Foreword of 2.1

--- chapter2.hs
+++ chapter2.hs
@@ -8,3 +8,5 @@
 -- To do this, run [:l filename.hs] in the ghci shell in the directory housing your code.
 
 -- Discontinuities between Haskell and Scheme at this point of SICP:
+-- Scheme has a special pair type, which is defined by the cons operator. Haskell, on the other hand, has no specific pair type, but rather has two compatible data types --> the tuple, and the list.
+-- In the following series of problems, I will do the best I can to use the corresponding data type. Note that the Tuple type can often be replaced by the List type, while the opposite may not necessarily be true.
