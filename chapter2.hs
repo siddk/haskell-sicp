893e920e0d487f78bb7a51aa4578bfce09415dfb
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-07 20:27:54 -0700

    Starting Chapter 2

---
+++ chapter2.hs
@@ -0,0 +1,10 @@
+-- Chapter 2 of A Structure and Interpretations of Computer Programs
+-- This file contains problem statements, and haskell solutions for selected problems.
+
+-- Notes on compiling the file: Like the Scheme workflow for SICP, this file does not need to
+-- be compiled... rather, just import the function definitions into the Haskell interpreter (ghci)
+-- to test your functions.
+
+-- To do this, run [:l filename.hs] in the ghci shell in the directory housing your code.
+
+-- Discontinuities between Haskell and Scheme at this point of SICP:
