b42a37d2e562da42553e6ba5299f55417245c8fd
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-16 23:54:43 -0700

    Working sameParity

--- chapter2.hs
+++ chapter2.hs
@@ -68,7 +68,7 @@ reverse' (x:xs) = (reverse xs) ++ [x]
 -- The procedures +, *, and list take arbitrary numbers of arguments. One way to define such procedures is to use define with dotted-tail notation. In a procedure definition, a parameter list that has a dot before the last parameter name indicates that, when the procedure is called, the initial parameters (if any) will have as values the initial arguments, as usual, but the final parameter's value will be a list of any remaining arguments.
 -- Use this notation to write a procedure same-parity that takes one or more integers and returns a list of all the arguments that have the same even-odd parity as the first argument.
 -- NOTE: Haskell does not support dotted tail notation, so this example will serve as an implementation of filter.
-sameParity :: [a] -> [a]
+sameParity :: (Integral a) => [a] -> [a]
 sameParity [] = []
 sameParity (x:xs) = if even x
                     then x : (filter' xs even)
