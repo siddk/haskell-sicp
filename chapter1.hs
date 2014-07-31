501fd6a2ea838c6f66b8879e736ec94428f15a2a
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-30 21:07:36 -0700

    Update Exercise 1.41

--- chapter1.hs
+++ chapter1.hs
@@ -2,8 +2,8 @@
 -- This file contains problem statements, and haskell solutions for selected problems.
 
 -- Notes on compiling the file: Like the Scheme workflow for SICP, this file does not need to
--- be compiled... rather, just import the function definitions into the Haskell interpreter (ghci)
--- to test your functions.
+-- be compiled... rather, just import the function definitions into the Haskell interpreter
+-- (ghci) to test your functions.
 --
 -- To do this, run [:l filename.hs] in the ghci shell in the directory housing your code.
 
@@ -25,7 +25,9 @@ sumSquares x y = (x^2) + (y^2)
 
 
 -- Exercise 1.5 (normal vs. applicative order)
--- Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is using applicative-order evaluation or normal-order evaluation. He defines the following two procedures:
+-- Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with
+-- is using applicative-order evaluation or normal-order evaluation. He defines the
+-- following two procedures:
 -- (define (p) (p))
 -- (define (test x y)
 --  (if (= x 0)
@@ -40,7 +42,9 @@ sumSquares x y = (x^2) + (y^2)
 
 
 -- Exercise 1.6
--- Alyssa P. Hacker doesn't see why if needs to be provided as a special form. ``Why can't I just define it as an ordinary procedure in terms of cond?'' she asks. Alyssa's friend Eva Lu Ator claims this can indeed be done, and she defines a new version of if:
+-- Alyssa P. Hacker doesn't see why if needs to be provided as a special form. ``Why can't I
+-- just define it as an ordinary procedure in terms of cond?'' she asks. Alyssa's friend Eva
+-- Lu Ator claims this can indeed be done, and she defines a new version of if:
 -- (define (new-if predicate then-clause else-clause)
 --  (cond (predicate then-clause)
 --        (else else-clause)))
@@ -57,14 +61,20 @@ sumSquares x y = (x^2) + (y^2)
 --                      x)))
 -- What happens when Alyssa attempts to use this to compute square roots? Explain.
 ---------------------------------------------------------------------------------
--- (SCHEME) new-if is a function (not a special form), and as Scheme uses applicative order evaluation, the arguments of the function are always evaluated first, so the second clause will continue to be evaluated, resulting in a loop.
--- (HASKELL) Haskell uses lazy evaluation, and as such, the function arguments will not be evaluated unless needed, so the program will run successfully.
+-- (SCHEME) new-if is a function (not a special form), and as Scheme uses applicative order
+-- evaluation, the arguments of the function are always evaluated first, so the second
+-- clause will continue to be evaluated, resulting in a loop.
+-- (HASKELL) Haskell uses lazy evaluation, and as such, the function arguments will not be
+-- evaluated unless needed, so the program will run successfully.
 
 
 -- Exercise 1.8
--- Newton's method for cube roots is based on the fact that if y is an approximation to the cube root of x, then a better approximation is given by the value
+-- Newton's method for cube roots is based on the fact that if y is an approximation to the
+-- cube root of x, then a better approximation is given by the value
 -- ((x/y^2)+2y) / 3
--- Use this formula to implement a cube-root procedure analogous to the square-root procedure. (In section 1.3.4 we will see how to implement Newton's method in general as an abstraction of these square-root and cube-root procedures.)
+-- Use this formula to implement a cube-root procedure analogous to the square-root
+-- procedure. (In section 1.3.4 we will see how to implement Newton's method in general as
+-- an abstraction of these square-root and cube-root procedures.)
 cubeRootIter :: (Num a, Ord a, Fractional a) => a -> a -> a
 cubeRootIter guess x = if (goodEnough guess x)
                        then guess
@@ -78,7 +88,14 @@ improve guess x = ((x / (guess * guess)) + (2 * guess)) / 3
 
 
 -- Exercise 1.16
---  Design a procedure that evolves an iterative exponentiation process that uses successive squaring and uses a logarithmic number of steps, as does fast-expt. (Hint: Using the observation that (bn/2)2 = (b2)n/2, keep, along with the exponent n and the base b, an additional state variable a, and define the state transformation in such a way that the product a bn is unchanged from state to state. At the beginning of the process a is taken to be 1, and the answer is given by the value of a at the end of the process. In general, the technique of defining an invariant quantity that remains unchanged from state to state is a powerful way to think about the design of iterative algorithms.)
+--  Design a procedure that evolves an iterative exponentiation process that uses successive
+-- squaring and uses a logarithmic number of steps, as does fast-expt. (Hint: Using the
+-- observation that (bn/2)2 = (b2)n/2, keep, along with the exponent n and the base b, an
+-- additional state variable a, and define the state transformation in such a way that the
+-- product a bn is unchanged from state to state. At the beginning of the process a is taken
+-- to be 1, and the answer is given by the value of a at the end of the process. In general,
+-- the technique of defining an invariant quantity that remains unchanged from state to
+-- state is a powerful way to think about the design of iterative algorithms.)
 fastExpt :: (Num a, Ord a) => a -> Int -> a
 fastExpt base n = fastExptIter 1 base n
 
@@ -90,7 +107,9 @@ fastExptIter cur base n = if even n
 
 
 -- Exercise 1.30
--- The sum procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. Show how to do this by filling in the missing expressions in the following definition:
+-- The sum procedure above generates a linear recursion. The procedure can be rewritten so
+-- that the sum is performed iteratively. Show how to do this by filling in the missing
+-- expressions in the following definition:
 -- (define (sum term a next b)
 --   (define (iter a result)
 --     (if <??>
@@ -105,7 +124,10 @@ iterSum term a next b = iter a 0
 
 
 -- Exercise 1.31
--- a.  The sum procedure is only the simplest of a vast number of similar abstractions that can be captured as higher-order procedures. Write an analogous procedure called product that returns the product of the values of a function at points over a given range. Show how to define factorial in terms of product.
+-- a.  The sum procedure is only the simplest of a vast number of similar abstractions that
+-- can be captured as higher-order procedures. Write an analogous procedure called product
+-- that returns the product of the values of a function at points over a given range. Show
+-- how to define factorial in terms of product.
 iterProduct :: (Num a, Ord a) => (a -> a) -> a -> (a -> a) -> a -> a
 iterProduct term a next b = iter a 1
     where iter a result = if a > b
@@ -115,7 +137,9 @@ iterProduct term a next b = iter a 1
 factorialProduct :: (Num a, Ord a) => a -> a
 factorialProduct n = iterProduct (\x -> x * 1) 1 (\x -> x + 1) n
 
--- b.  If your product procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
+-- b.  If your product procedure generates a recursive process, write one that generates an
+-- iterative process. If it generates an iterative process, write one that generates a
+-- recursive process.
 recursiveProduct :: (Num a, Ord a) => (a -> a) -> a -> (a -> a) -> a -> a
 recursiveProduct term a next b = if a > b
                                  then 1
@@ -123,11 +147,19 @@ recursiveProduct term a next b = if a > b
 
 
 -- Exercise 1.32
--- Show that sum and product (exercise 1.31) are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general accumulation function:
+-- Show that sum and product (exercise 1.31) are both special cases of a still more general
+-- notion called accumulate that combines a collection of terms, using some general
+-- accumulation function:
 -- (accumulate combiner null-value term a next b)
--- Accumulate takes as arguments the same term and range specifications as sum and product, together with a combiner procedure (of two arguments) that specifies how the current term is to be combined with the accumulation of the preceding terms and a null-value that specifies what base value to use when the terms run out. Write accumulate and show how sum and product can both be defined as simple calls to accumulate.
+-- Accumulate takes as arguments the same term and range specifications as sum and product,
+-- together with a combiner procedure (of two arguments) that specifies how the current term
+-- is to be combined with the accumulation of the preceding terms and a null-value that
+-- specifies what base value to use when the terms run out. Write accumulate and show how
+-- sum and product can both be defined as simple calls to accumulate.
 
--- (HASKELL) It's is important to note at this time that haskell has two functions foldr and foldl that will essentially apply accumulate over a LIST, not a range of two values. As such, we will define accumulate as it appears here in haskell as follows:
+-- (HASKELL) It's is important to note at this time that haskell has two functions foldr and
+-- foldl that will essentially apply accumulate over a LIST, not a range of two values. As
+-- such, we will define accumulate as it appears here in haskell as follows:
 accumulate :: (Ord a) => (a -> a -> a) -> a -> (a -> a) -> a -> (a -> a) -> a -> a
 accumulate combiner null_value term x next n = iter x null_value
     where iter x result = if x > n
@@ -136,7 +168,12 @@ accumulate combiner null_value term x next n = iter x null_value
 
 
 -- Exercise 1.33
--- You can obtain an even more general version of accumulate (exercise 1.32) by introducing the notion of a filter on the terms to be combined. That is, combine only those terms derived from values in the range that satisfy a specified condition. The resulting filtered-accumulate abstraction takes the same arguments as accumulate, together with an additional predicate of one argument that specifies the filter. Write filtered-accumulate as a procedure. Show how to express the following using filtered-accumulate:
+-- You can obtain an even more general version of accumulate (exercise 1.32) by introducing
+-- the notion of a filter on the terms to be combined. That is, combine only those terms
+-- derived from values in the range that satisfy a specified condition. The resulting
+-- filtered-accumulate abstraction takes the same arguments as accumulate, together with an
+-- additional predicate of one argument that specifies the filter. Write filtered-accumulate
+-- as a procedure. Show how to express the following using filtered-accumulate:
 -- a. the sum of the squares of the odd numbers in the interval a to b
 filteredAccumulate :: Ord a => (a -> Bool) -> (a -> a -> a) -> a -> (a -> a) -> a -> (a -> a) -> a -> a
 filteredAccumulate predicate combiner null_val term x next n = iter x null_val
@@ -158,17 +195,23 @@ sumOddSquares a b = filteredAccumulate odd (+) 0 (\x -> x * x) a (\x -> x + 1) b
 -- 4
 -- (f (lambda (z) (* z (+ z 1))))
 -- 6
--- What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? Explain.
+-- What happens if we (perversely) ask the interpreter to evaluate the combination (f f)?
+-- Explain.
 -----------------------------------------------------------------------------------
--- (SCHEME + HASKELL) With applicative order evaluation and lazy evaluation, the order of function calls looks like this:
+-- (SCHEME + HASKELL) With applicative order evaluation and lazy evaluation, the order of
+-- function calls looks like this:
 -- (f f) --> (f 2) --> (2 2)
 -- This results in an error calling the number 2 as a function
 
 
 -- Exercise 1.41
--- Define a procedure double that takes a procedure of one argument as argument and returns a procedure that applies the original procedure twice. For example, if inc is a procedure that adds 1 to its argument, then (double inc) should be a procedure that adds 2. What value is returned by
+-- Define a procedure double that takes a procedure of one argument as argument and returns
+-- a procedure that applies the original procedure twice. For example, if inc is a procedure
+-- that adds 1 to its argument, then (double inc) should be a procedure that adds 2. What
+-- value is returned by
 -- (((double (double double)) inc) 5) --> 2^4 --> + 16 --> 21 (QED)
--- (NOTE) Whereas Scheme would use nested parentheses, haskell has specific function operators, which will be used here:
+-- (NOTE) Whereas Scheme would use nested parentheses, haskell has specific function
+-- operators, which will be used here:
 -- The first operator is the $ --> Defers evaluation order --> f(g(x)) --> f(g) $ x
 -- The second operator is the . --> function composition f(g(x)) --> (f . g) x
 double :: (a -> a) -> (a -> a)
@@ -176,14 +219,22 @@ double procedure = procedure . procedure
 
 
 -- Exercise 1.42
--- Let f and g be two one-argument functions. The composition f after g is defined to be the function x -> f(g(x)). Define a procedure compose that implements composition.
+-- Let f and g be two one-argument functions. The composition f after g is defined to be the
+-- function x -> f(g(x)). Define a procedure compose that implements composition.
 -- (NOTE) This is a trivial problem with the . operator
 compose :: (b -> c) -> (a -> b) -> (a -> c) -- Note transitivity of type signature
 compose f g = f . g
 
 
 -- Exercise 1.43
--- If f is a numerical function and n is a positive integer, then we can form the nth repeated application of f, which is defined to be the function whose value at x is f(f(...(f(x))...)). For example, if f is the function x   x + 1, then the nth repeated application of f is the function x   x + n. If f is the operation of squaring a number, then the nth repeated application of f is the function that raises its argument to the 2nth power. Write a procedure that takes as inputs a procedure that computes f and a positive integer n and returns the procedure that computes the nth repeated application of f.
+-- If f is a numerical function and n is a positive integer, then we can form the nth
+-- repeated application of f, which is defined to be the function whose value at x is
+-- f(f(...(f(x))...)). For example, if f is the function x   x + 1, then the nth repeated
+-- application of f is the function x   x + n. If f is the operation of squaring a number,
+-- then the nth repeated application of f is the function that raises its argument to the
+-- 2nth power. Write a procedure that takes as inputs a procedure that computes f and a
+-- positive integer n and returns the procedure that computes the nth repeated application
+-- of f.
 repeatedProc :: (Eq a, Num a) => (b -> b) -> a -> (b -> b)
 repeatedProc f 1 = f
 repeatedProc f n = f . repeatedProc f (n - 1)
